8bb9515771d7892dfdfc057c25b2e4df40fdc792
chrisgay <chris.gay@cmg2.co>
2013-09-27 12:52:25 -0500

    images

--- app/controllers/users_controller_controller.rb
+++ app/controllers/users_controller_controller.rb
@@ -1,2 +1,15 @@
-class UsersControllerController < ApplicationController
+class UsersControllerController < ApplicationController   
+    
+    def create
+      # Create the user from params
+      @user = User.new(params[:user])
+      if @user.save
+        # Deliver the signup_email
+        Notifier.signup_email(@user).deliver
+        redirect_to(@user, :notice => 'User created')
+      else
+        render :action => 'new'
+      end
+    end    
+  
 end
